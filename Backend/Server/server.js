bd5a77f615b987cfa82d5ced16c83952ec86a627
suresh <nandhyalasuresh143@gmail.com>
2025-09-01 10:55:57 +0530

    updated

--- Backend/Server/server.js
+++ Backend/Server/server.js
@@ -14,14 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Use environment variable for frontend origin
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: 'https://master-mind-lovat.vercel.app',
+  credentials: true
+}));
 
 // MongoDB Connection
 const mongoUri = process.env.MONGODB_URI;
